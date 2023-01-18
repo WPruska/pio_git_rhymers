df6fa0e4207ce2d3ce229ed7bf3500c4d40e2a12
Weronika Pruska <236438@edu.p.lodz.pl>
2023-01-18 15:20:08 +0100

    3.2 Zmiana nazw metod

--- src/main/java/edu/kis/vh/nursery/factory/Rhymersfactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/Rhymersfactory.java
@@ -3,13 +3,13 @@ package edu.kis.vh.nursery.factory;
 import edu.kis.vh.nursery.DefaultCountingOutRhymer;
 
 public interface RhymersFactory {
-	
-	public DefaultCountingOutRhymer GetStandardRhymer();
 
-	public DefaultCountingOutRhymer GetFalseRhymer();
+	public DefaultCountingOutRhymer getStandardRhymer();
 
-	public DefaultCountingOutRhymer GetFIFORhymer();
+	public DefaultCountingOutRhymer getFalseRhymer();
 
-	public DefaultCountingOutRhymer GetHanoiRhymer();
+	public DefaultCountingOutRhymer getFIFORhymer();
+
+	public DefaultCountingOutRhymer getHanoiRhymer();
 
 }
